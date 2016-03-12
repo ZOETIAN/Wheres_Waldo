9f14f85a18fd6318bbd7ba4ead2ffc0c87f02d97
Wanru Tian <w3tian@uwaterloo.ca>
2016-03-11 19:10:39 -0500

    Update tmp.js

--- tmp.js
+++ tmp.js
@@ -1,65 +1,15 @@
-var svg = document.getElementById('svg');
-var hole = document.getElementById('#hole');
-var rect = document.getElementById('#rect');
-var rectTop = document.getElementById('#rect-top');
-var path = document.getElementById('#path');
-var gradInvert = document.getElementById('#rad-grad-invert');
 
-var holeR = 100;
+var $waldo = $('div');
 
-var svg.clientWidth = 0;
-var svg.clientHeight = 0;
+$(document).mousemove(function(e){
+  var x = e.pageX;
+  var y = e.pageY;
 
-var svgW = svg.clientWidth;
-var svgH = svg.clientHeight;
+  var path = 'circle(20% at ' + x + 'px ' + y + 'px)';
 
-var svgHorcenter = svgW / 2;
-
-setCoords();
-
-function setCoords(event) {
-
-  var cx = hole.cx.baseVal.value;
-  var cy = hole.cy.baseVal.value;
-
-  if (event) {
-    cx = event.offsetX;
-    cy = event.offsetY;
-  }
-
-  hole.setAttr({
-    'cx': cx,
-    'cy': cy
+  window.pathpath = path;
+  $waldo.css({
+    '-webkit-clip-path' : path,
+    'clip-path' : path
   });
-
-  gradInvert.setAttr({
-    'cx': cx,
-    'cy': cy
-  });
-
-  var pathCoordsList = [
-    cx - holeR, cy,
-    cx + holeR, cy,
-    svgHorcenter, svgH
-  ];
-
-  var pathCoords = pathCoordsList.join(' ');
-
-  path.setAttr({
-    'd': 'M' + pathCoords
-  });
-}
-
-rectTop.onmousemove = function(event) {
-  setCoords(event);
-}
-
-window.onresize = function() {
-  svgW = svg.clientWidth;
-  svgH = svg.clientHeight;
-
-  svgHorcenter = svgW / 2;
-
-  setCoords();
-}
-
+});
