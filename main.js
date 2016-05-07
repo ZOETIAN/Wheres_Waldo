112bb0fbd9a67706abf509a29b53434ae5154d4c
w3tian <w3tian@uwaterloo.ca>
2016-05-07 19:07:41 -0400

    initial push

--- main.js
+++ main.js
@@ -23,16 +23,44 @@ $(document).dblclick(function(e){
     , 'url(http://img01.thedrum.com/s3fs-public/drum_basic_article/97639/main_images/WheresWallyAtWembley_6.jpg)'
   ];
 
-  if(mouseX > 1650 && mouseX < 1690 && mouseY > 215 && mouseY < 290) {
-    $('body').css("background", "#FFEC8B");
-    level += 1;
-    level = level % backgrounds.length;
-    $('html').css('background-image', backgrounds[level]);
-    $('.main_div').css('background-image', backgrounds[level]);
-    alert("You find the Waldo! You can now continue the level " + (level+1));
-  } else{
-    $('body').css("background", "#B0E2FF");
-    alert("Really?! That's your Waldo!");
-  }
-});
+  var windowWidth = $('window').width(),
+      windowHeight = $('window').height(),
+      oriImageWidths = [2048, 2828, 2048, 2953],  // for four images
+      oriImageHeights = [1454, 1828, 1282, 2088], // for four images
+
+      // oriWaldoWidths = [[1725, 1750, 1725, 1750],[1125, 1155, 1130, ],[], []],  //
+      // oriWaldoHeights = [[410, 450, 410, 450], [1035, 1140, 1190, ],[], []];     // first Waldo image: leftTop, rightTop, leftBot, rightBot
+
+      oriWaldoCoors = [
+                       [1725,410],[1750,450],
+                       [1125,1035],[1136,1216],
+                       [980, 1130], [1030, 1190],
+                       [285, 1690], [320, 1760]
+      ];
+
+      // calculate the ratios for four images
+      for(var i = 0;i < 4; i++) {
+        var heightRatios = [], widthRatios = [];
+        heightRatios.push(oriWaldoCoors[i][0] / oriImageHeights[i]);
+        widthRatios.push(oriWaldoCoors[i][1] / oriImageWidths[i]);
+      }
+
+      // detect if the mouse is within the Waldo_range: if found, change the background color and next level
+      var j = level * 2;
+      if(mouseX >= oriWaldoCoors[j][0] && mouseX <= oriWaldoCoors[j+1][0] && mouseY >= oriWaldoCoors[j][1] && mouseY <= oriWaldoCoors[j+1][1]) {
+        $('body').css("background", "#FFEC8B");
+        level += 1;
+        level = level % backgrounds.length;
+        $('html').css('background-image', backgrounds[level]);
+        $('.main_div').css('background-image', backgrounds[level]);
+        alert("You find the Waldo! You can now continue the level " + (level+1));
+        $('body').css("background", "#000");
+      }
+      else{
+        $('body').css("background", "#B0E2FF");
+        alert("Really?! That's your Waldo!")
+        $('body').css("background", "#000");
+      }
+
+});// double click funtion to find waldo
 
